fa0f9ce03dbb696ed2e193b58347645378df9b24
AsraSyahrastani <asra.s.ft20@mail.umy.ac.id>
2022-01-04 14:51:00 +0700

    PraktikumTableController

---
+++ src/main/java/TableProjec/Praktikum/ProjecController.java
@@ -0,0 +1,70 @@
+package TableProjec.Praktikum;
+
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+@Controller
+
+public class ProjecController {
+
+    @RequestMapping("/input")
+    public String getData(HttpServletRequest data, Model discountprocess){
+
+        String inputname = data.getParameter("var_name");
+
+        String inputprice = data.getParameter("var_price");
+
+        String inputquantity = data.getParameter("var_quantity");
+        
+        String inputUang = data.getParameter("var_uang");
+
+        String diskon = "";
+
+        Double iUang = Double.valueOf(inputUang);
+        Double iPrice = Double.valueOf(inputprice);
+        Double iTotal = Double.valueOf(inputquantity);
+        Double PriceTotal = iPrice * iTotal;
+        Double getTotal = null;
+
+        if (PriceTotal < 16000)
+        {
+            getTotal = PriceTotal - (0 * PriceTotal/100);
+            diskon = "0%";
+        }
+        else if (PriceTotal >= 16000 && PriceTotal < 25000)
+        {
+            getTotal = PriceTotal - (10 * PriceTotal/100);
+            diskon = "5%";
+        }
+        else if (PriceTotal >= 25000)
+        {
+            getTotal = PriceTotal - (15 * PriceTotal/100);
+            diskon = "5%";
+        }
+        
+        Double kembalian = iUang - getTotal;
+
+        discountprocess.addAttribute("name", inputname);
+        discountprocess.addAttribute("price", inputprice);
+        discountprocess.addAttribute("quantity", inputquantity);
+        discountprocess.addAttribute("total", getTotal);
+        discountprocess.addAttribute("dc", diskon);
+        discountprocess.addAttribute("duit", inputUang);
+        discountprocess.addAttribute("kmb", kembalian);
+        
+        return "view";
+    }
+}
---
+++ src/test/java/TableProjec/Praktikum/PraktikumApplicationTests.java
@@ -0,0 +1,13 @@
+package TableProjec.Praktikum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
